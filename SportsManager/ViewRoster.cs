d73dc1bf1e62884b18d672f9d3b8776dd6c57e55
Jacob Blankenburg <jblankenburg2015@aol.com>
2016-02-25 17:21:53 -0600

    Fixed switch case for viewing roster

--- SportsManager/ViewRoster.cs
+++ SportsManager/ViewRoster.cs
@@ -13,7 +13,7 @@ namespace SportsManager
 
         public void ViewPlayerRoster()
         {
-            Console.WriteLine(" Choose 1 to view the roster \n Choose 2 end program\n Choose 4 to go to Menu");
+            Console.WriteLine(" Choose 1 to view the roster \n Choose 2 end program\n Choose 3 to go to Menu");
 
             do
             {
